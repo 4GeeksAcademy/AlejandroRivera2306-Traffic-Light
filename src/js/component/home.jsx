480050db81f7db16be84ba0094664deaa95f15eb
Alejandro Rivera <124119414+AlejandroRivera2306@users.noreply.github.com>
2023-05-09 03:28:17 +0000

    primer-cambio

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [oneColor,setOneColor]= useState('rgb(168, 54, 54)')
+	const [twoColor,setTwoColor]= useState('rgb(188, 188, 77)')
+	const [threeColor,setThreeColor]= useState('rgb(79, 166, 79)')
+
+
+	function onecolor(){
+		if(oneColor == 'rgb(168, 54, 54)'){
+			setOneColor('rgb(255, 0, 0)')
+
+		}else {
+
+			setOneColor('rgb(168, 54, 54)')
+		}
+			// setOneColor('gray')
+			//apagar boton 2 y tres 
+			setTwoColor('rgb(188, 188, 77)')
+			setThreeColor('rgb(79, 166, 79)')
+	};
+
+	function twocolor(){
+		if(twoColor == 'rgb(188, 188, 77)'){
+			setTwoColor('rgb(255, 255, 0)')
+
+		}else {
+
+			setTwoColor('rgb(188, 188, 77)')
+		}
+		//apagar boton 1 y tres
+			setOneColor('rgb(168, 54, 54)')
+			setThreeColor('rgb(79, 166, 79)')
+
+			//setTwoColor('gray')
+	};
+
+	function threecolor(){
+		if(threeColor == 'rgb(79, 166, 79)'){
+			setThreeColor('rgb(3, 245, 3)')
+
+		}else {
+
+			setThreeColor('rgb(79, 166, 79)')
+		}
+		//apagar boton 1 y dos 
+		setOneColor('rgb(168, 54, 54)')
+		setTwoColor('rgb(188, 188, 77)')
+
+			//setThreeColor('gray')
+	};
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+	<div className="contenedor">
+		<button className="semaforo" style={{backgroundColor: oneColor}} onClick={onecolor}></button>
+    	<button className="semaforo" style={{backgroundColor: twoColor}} onClick={twocolor}></button>
+		<button className="semaforo" style={{backgroundColor: threeColor}} onClick={threecolor}></button>
+	</div>
+	
+
+	
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,15 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+
+const Contenedor = () => {
+    return (
+      <div className="menu">
+        <Home/>
+      </div>
+    );
+  };
+
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<Contenedor />, document.querySelector("#app"));
